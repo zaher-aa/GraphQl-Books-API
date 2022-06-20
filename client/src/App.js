06e6f12176af44009276fa4ba2f11548754ba614
zaher-aa <zaherabuamro@gmail.com>
2022-06-20 11:30:17 +0300

    used graphql from the client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,84 @@
+import { useState, useEffect } from 'react';
+import { gql, useQuery, useMutation } from '@apollo/client';
+
 function App() {
-  return <>Hello World</>;
+  const [bookName, setBookName] = useState('');
+  const [bookPages, setBookPages] = useState('');
+  const [bookAuthorId, setBookAuthorId] = useState('');
+  const [addBook, { data: addedBookData }] = useMutation(gql`
+    mutation ($input: BookInput!) {
+      addBook(input: $input) {
+        id
+        name
+      }
+    }
+  `);
+
+  const { loading, error, data } = useQuery(gql`
+    {
+      books {
+        name
+        pages
+      }
+    }
+  `);
+
+  useEffect(() => {
+    console.log('addedBookData', addedBookData);
+  }, [addedBookData]);
+
+  return (
+    <>
+      <div>
+        {loading
+          ? 'fetching data...'
+          : data.books.map((book, i) => {
+              return (
+                <div key={i}>
+                  <h1>{book.name}</h1>
+                  <p>Pages: {book.pages}</p>
+                </div>
+              );
+            })}
+      </div>
+      <div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+
+            console.log(bookName, +bookPages, +bookAuthorId);
+
+            addBook({
+              variables: {
+                input: {
+                  name: bookName,
+                  pages: +bookPages,
+                  authorId: +bookAuthorId,
+                },
+              },
+            });
+          }}
+        >
+          <input
+            type="text"
+            placeholder="book name"
+            onChange={(e) => setBookName(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="book pages"
+            onChange={(e) => setBookPages(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="book author id"
+            onChange={(e) => setBookAuthorId(e.target.value)}
+          />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
